daf474b44c633ed960b080830972d7b3202a9d45
gbelot2003 <gbelot2003@hotmail.com>
2015-03-28 03:32:28 +0000

    First commit

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function clibre_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function clibre_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function clibre_preprocess_node(&$variables) {
+}
